cd0890c9bda2cd1e6c709ddb6e1a2ae837a0e80d
Keith Dumont <keithdumont@Keiths-MacBook-Pro.local>
2020-05-19 10:22:19 -0700

    search track

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -2,43 +2,20 @@ import React from "react";
 
 const SearchForm = (props) => {
   return (
-    <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-        >
-          <small id="emailHelp" class="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </input>
+    <form className="search-track">
+      <div className="form-group">
+        <input type="text" className="form-control" placeholder="Song" />
       </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-          placeholder="Password"
-        ></input>
-      </div>
-      <div class="form-check">
-        <input
-          type="checkbox"
-          class="form-check-input"
-          id="exampleCheck1"
-        ></input>
-        <label class="form-check-label" for="exampleCheck1">
-          Check me out
-        </label>
+      <div className="form-group">
+        <input type="text" className="form-control" placeholder="Artist" />
       </div>
 
-      <button type="submit" class="btn btn-primary">
-        Submit
+      <button
+        type="submit"
+        className="btn btn-primary"
+        onClick={props.searchTrack}
+      >
+        Search
       </button>
     </form>
   );
